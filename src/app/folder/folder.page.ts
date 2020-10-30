fcd1c3d109a80cb8291ecb08245a8a15561b0b8a
EloyCabezuelo <eloy.cabezuelo@aktios.com>
2020-10-30 22:57:48 +0100

    dasda

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PhotoService } from '../services/photo.service';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-folder',
@@ -10,10 +10,29 @@ import { PhotoService } from '../services/photo.service';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute, public photoService: PhotoService) { }
+  constructor(private activatedRoute: ActivatedRoute, private camera: Camera) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+      // Add new photo to gallery
+/*       this.photos.unshift({
+        data: 'data:image/jpeg;base64,' + imageData
+      }); */
+
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue: " + err);
+    });
+  }
 }
